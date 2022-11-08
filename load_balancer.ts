5be1b7dabe9c8a5aedb252980eff490cb71c24c1
hemangahuja <hemangahujaeotw@gmail.com>
2022-11-08 11:39:25 +0530

    added 4 algorithms

--- load_balancer.ts
+++ load_balancer.ts
@@ -2,25 +2,69 @@ import express from "express";
 import axios from "axios";
 import path from "path";
 
-const [basePort, count] = process.argv.slice(2);
-
+const [basePort, _count, algorithm] = process.argv.slice(2);
+const count = Number(_count);
 const app = express();
 
 let current = 0;
+let requestCountCurrent = 0;
+
+const hashCode = (str: string): number => {
+    let hash = 0;
+    if (str.length === 0) return hash;
+    for (let i = 0; i < str.length; i++) {
+        const chr = str.charCodeAt(i);
+        hash = (hash << 5) - hash + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+const getServer = (options?: object): number => {
+    switch (algorithm) {
+        case "round-robin": {
+            return current++ % count;
+        }
+        case "weighted-round-robin": {
+            const limit = count - current;
+            requestCountCurrent++;
+            if (requestCountCurrent > limit) {
+                requestCountCurrent = 0;
+                current++;
+            }
+            return current % count;
+        }
+        case "random": {
+            return Math.round(Math.random() * (count - 1));
+        }
+        case "custom": {
+            const hash = hashCode(JSON.stringify(options)) % count;
+            return hash < 0 ? count + hash : hash;
+        }
+    }
+    return 0;
+};
 
 const handler = async (req: express.Request, res: express.Response) => {
-    const { method, url, headers, body } = req;
-
+    const { method, headers, body, url, query } = req;
+    const data = {
+        url,
+        method: method,
+        headers: headers,
+        data: body,
+        query,
+    };
     const server = `http://localhost:${
-        Number(basePort) + (current++ % Number(count))
+        Number(basePort) +
+        getServer({
+            query,
+            method,
+            body,
+        })
     }`;
     try {
-        const response = await axios({
-            url: `${server}${url}`,
-            method: method,
-            headers: headers,
-            data: body,
-        });
+        data.url = `${server}${url}`;
+        const response = await axios(data);
         res.send(response.data);
     } catch (err) {
         console.log(err);
@@ -30,6 +74,7 @@ const handler = async (req: express.Request, res: express.Response) => {
 app.get("/favicon.ico", (req, res) =>
     res.sendFile(path.join(__dirname, "/favicon.ico"))
 );
+
 app.use(handler);
 
 app.listen(8080, () => console.log("Load balancer running at port 8080"));
--- server.ts
+++ server.ts
@@ -1,13 +1,38 @@
 import express from "express";
+import { generateKeyPair } from "crypto";
 
 const [basePort, count] = process.argv.slice(2);
 
 const apps = new Array(Number(count)).fill(null).map(($) => express());
+const heavyComputation = async (): Promise<any> => {
+    return new Promise((resolve) => {
+        generateKeyPair(
+            "rsa",
+            {
+                modulusLength: 1024,
+                publicKeyEncoding: {
+                    type: "spki",
+                    format: "pem",
+                },
+                privateKeyEncoding: {
+                    type: "pkcs8",
+                    format: "pem",
+                },
+            },
+            (err: Error | null, publicKey: string, privateKey: string) => {
+                if (err) return err;
+                resolve(publicKey + privateKey);
+            }
+        );
+    });
+};
 
 const handler =
-    (num: number) => (req: express.Request, res: express.Response) => {
+    (num: number) => async (req: express.Request, res: express.Response) => {
         const { method, url, headers, body } = req;
-        res.send("Response from server " + num).status(200);
+
+        const ress = await heavyComputation();
+        res.send("Response from server " + num + ress).status(200);
     };
 
 apps.forEach((app, idx) => {
